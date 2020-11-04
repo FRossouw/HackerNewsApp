8e63ef4d40e281d594b9277c12638861daf0559e
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-11-04 13:21:59 +0200

    Refactor code

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { select, Store } from '@ngrx/store';
-import { Server } from 'http';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { Story } from 'src/app/models/story';
 import { NewsReaderService } from 'src/app/services/news-reader.service';
-import { selectTopStories, State } from 'src/app/store/reducers/news.reducer';
+import { NewsState } from 'src/app/store/reducers/news.reducer';
 
 @Component({
   selector: 'app-home',
@@ -15,19 +13,9 @@ import { selectTopStories, State } from 'src/app/store/reducers/news.reducer';
 export class HomeComponent implements OnInit {
 
   topStories$: Observable<Story[]>;
-  constructor(private newsStore: Store<State>, private service: NewsReaderService) { }
+  constructor(private newsStore: Store<NewsState>, private service: NewsReaderService) { }
 
   ngOnInit(): void {
-    this.topStories$ = this.newsStore.pipe(
-      select(selectTopStories)
-    );
-
-    let coms = this.service.getComments(['2922097', '2922429', '2924562', '2922709', '2922573']);
-    console.log(coms)
-
-    let sstory = this.service.getStories(['9129911', '9129199', '9127761', '9128141', '9128264']);
-    console.log(sstory)
-
 
   }
 
