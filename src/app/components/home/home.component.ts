5343ef1c16cfc8a8e65f43d9d62b7b0d2b25d4c7
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-11-05 10:43:38 +0200

    Add home component details

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { Story } from 'src/app/models/story';
-import { NewsReaderService } from 'src/app/services/news-reader.service';
-import { NewsState } from 'src/app/store/reducers/news.reducer';
+import { getStories, getStoriesComplete, getTopStories } from 'src/app/store/actions/news.actions';
+import { NewsState, selectStories, selectTopStories, selectLoading } from 'src/app/store/reducers/news.reducer';
 
 @Component({
   selector: 'app-home',
@@ -12,11 +12,40 @@ import { NewsState } from 'src/app/store/reducers/news.reducer';
 })
 export class HomeComponent implements OnInit {
 
-  topStories$: Observable<Story[]>;
-  constructor(private newsStore: Store<NewsState>, private service: NewsReaderService) { }
+  loading$: Observable<boolean>;
+  stories$: Observable<Story[]>;
+  topStories$: Observable<string[]>;
+  topStoriesCount: number = 10;
+  constructor(private newsStore: Store<NewsState>) { }
 
   ngOnInit(): void {
+    this.loading$ = this.newsStore.pipe(
+      select(selectLoading)
+    );
+
+    this.stories$ = this.newsStore.pipe(
+      select(selectStories)
+    );
+
+    this.topStories$ = this.newsStore.pipe(
+      select(selectTopStories)
+    );
+
+    // Get Top Stories
+    this.newsStore.dispatch(getTopStories());
+    var tempStories = [];
+
+    this.topStories$.subscribe((stories) => {
+      if ((stories) && (stories.length > 0)) {
+        this.topStoriesCount = Math.min(10, stories.length);
+        for (let i = 0; i < Math.min(10, stories.length); i++) {
+          if (stories[i]) {
+            tempStories.push(stories[i].toString());
+          }
+        } // end of for
+        this.newsStore.dispatch(getStories({ storyIds: tempStories, storyList: [] }));
+      } // end of if stories and stories.length
+    }); // end of subscribe
 
   }
-
 }
