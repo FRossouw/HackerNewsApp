c27010a97dee7802dfd73823915bb4a8ff5eaa82
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-11-05 13:36:47 +0200

    Add item component

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Story } from 'src/app/models/story';
+import { Comment } from 'src/app/models/comment';
+import { getComments, getStory, getStoryComplete } from 'src/app/store/actions/news.actions';
+import { NewsState, selectComments, selectLoading, selectStories, selectStory } from 'src/app/store/reducers/news.reducer';
 
 @Component({
   selector: 'app-item',
@@ -7,9 +14,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
-  constructor() { }
+  id: string = '';
+  loading$: Observable<boolean>;
+  story$: Observable<Story>;
+  comments$: Observable<Comment[]>;
+  constructor(private activatedroute: ActivatedRoute, private newsStore: Store<NewsState>) { }
 
   ngOnInit(): void {
+    this.activatedroute.paramMap.subscribe(params => {
+      this.id = params.get('id');
+    });
+
+    this.loading$ = this.newsStore.pipe(
+      select(selectLoading)
+    );
+
+    this.story$ = this.newsStore.pipe(
+      select(selectStory)
+    );
+
+    this.comments$ = this.newsStore.pipe(
+      select(selectComments)
+    );
+
+    // Get Story
+    let tempComments = [];
+    this.newsStore.dispatch(getStory({ storyID: this.id }));
+
+    this.story$.subscribe((response) => {
+
+      if (this.id && response) {
+        if (response.kids) {
+          for (let index = 0; index < Math.min(10, response.kids.length); index++) {
+            if (response.kids[index]) {
+              tempComments.push(response.kids[index].toString());
+            }
+          }
+          this.newsStore.dispatch(getComments({ commentIds: tempComments, comments: [] }));
+        }
+      }
+
+    });
+
+
   }
 
 }
